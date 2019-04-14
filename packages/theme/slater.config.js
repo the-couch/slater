17e4d25aa46b2125a0f9a8ca1f989863104680ef
estrattonbailey <ericstrattonbailey@gmail.com>
2019-04-14 18:05:23 -0400

    update docs with default alias, remove default config

--- packages/theme/slater.config.js
+++ packages/theme/slater.config.js
@@ -1,17 +1,6 @@
 const path = require('path')
 
-/**
- * default config
- */
 module.exports = {
-  in: '/src',
-  out: '/build',
-  assets: {
-    in: '/src/scripts/index.js',
-    alias: {
-      '@': path.join(process.cwd(), '/src/scripts')
-    }
-  },
   themes: {
     development: {
       id: '',
