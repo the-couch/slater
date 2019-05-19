85769e76d8215531692a6bf3a0f5b1c3bd7bf1d2
Tomasz Bujnowicz <tomasz@prollective.com>
2019-05-19 11:46:42 +0100

    Fix Product page: Quantity buttons don't work

---
+++ packages/theme/src/scripts/components/product-counter.js
@@ -0,0 +1,30 @@
+import { component } from 'picoapp'
+
+export default component((node, ctx) => {
+
+  const decrease = node.querySelector('.js-counter-remove')
+  const increase = node.querySelector('.js-counter-add')
+  const quantity = node.querySelector('.js-counter-quantity')
+
+  const min = parseInt(quantity.attributes.min.value)
+  const max = parseInt(quantity.attributes.max.value)
+
+  let count = parseInt(quantity.value)
+
+  const set = (i) => {
+    count = Math.max(min, Math.min(i, max || 10000))
+    quantity.value = count
+  }
+
+  decrease.addEventListener('click', e => {
+    e.preventDefault()
+    console.log('decrease')
+    set(--count)
+  })
+
+  increase.addEventListener('click', e => {
+    e.preventDefault()
+    console.log('increase')
+    set(++count)
+  })
+})
