8014b4a0b0f929801fda41ebd945266f11551e0c
Ksenia <kseniacold@gmail.com>
2020-12-22 13:57:32 -0800

    Run prettier

--- packages/compiler/lib/createConfig.js
+++ packages/compiler/lib/createConfig.js
@@ -1,17 +1,17 @@
-const path = require('path')
-const clone = require('clone')
-const webpack = require('webpack')
+const path = require("path");
+const clone = require("clone");
+const webpack = require("webpack");
 
-const cwd = process.cwd()
+const cwd = process.cwd();
 
 const baseConfig = {
   output: {
-    filename: '[name].js'
+    filename: "[name].js",
   },
-  mode: 'development',
-  target: 'web',
+  mode: "development",
+  target: "web",
   performance: { hints: false },
-  devtool: 'inline-cheap-module-source-map',
+  devtool: "inline-cheap-module-source-map",
   module: {
     rules: [
       {
@@ -19,38 +19,38 @@ const baseConfig = {
         exclude: /node_modules/,
         use: [
           {
-            loader: require.resolve('babel-loader'),
+            loader: require.resolve("babel-loader"),
             options: {
-              sourceType: 'unambiguous',
+              sourceType: "unambiguous",
               plugins: [
-                require.resolve('@babel/plugin-syntax-object-rest-spread'),
-                require.resolve('@babel/plugin-proposal-class-properties'),
-                require.resolve('@babel/plugin-transform-runtime'),
+                require.resolve("@babel/plugin-syntax-object-rest-spread"),
+                require.resolve("@babel/plugin-proposal-class-properties"),
+                require.resolve("@babel/plugin-transform-runtime"),
               ],
               presets: [
-                require.resolve('@babel/preset-env'),
-                require.resolve('@babel/preset-react')
-              ]
-            }
-          }
-        ]
-      }
-    ]
+                require.resolve("@babel/preset-env"),
+                require.resolve("@babel/preset-react"),
+              ],
+            },
+          },
+        ],
+      },
+    ],
   },
   resolve: {
     alias: {
-      '@': process.cwd()
-    }
+      "@": process.cwd(),
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
 
-module.exports = function createConfig (conf, watch) {
-  const wc = clone(baseConfig)
+module.exports = function createConfig(conf, watch) {
+  const wc = clone(baseConfig);
 
   wc.entry = {
-    [path.basename(conf.in, '.js')]: path.resolve(cwd, conf.in)
-  }
+    [path.basename(conf.in, ".js")]: path.resolve(cwd, conf.in),
+  };
 
   /**
    * merge output as an object,
@@ -58,40 +58,41 @@ module.exports = function createConfig (conf, watch) {
    */
   wc.output = Object.assign(
     wc.output,
-    typeof conf.out === 'object' ? conf.out : {
-      path: path.resolve(cwd, conf.out)
-    }
-  )
+    typeof conf.out === "object"
+      ? conf.out
+      : {
+          path: path.resolve(cwd, conf.out),
+        }
+  );
 
-  wc.resolve.alias = Object.assign(wc.resolve.alias, conf.alias || {})
+  wc.resolve.alias = Object.assign(wc.resolve.alias, conf.alias || {});
 
-  wc.plugins = wc.plugins.concat([
-    new webpack.DefinePlugin(conf.env || {}),
-    conf.banner && new webpack.BannerPlugin({
-      banner: conf.banner,
-      raw: true,
-      entryOnly: true,
-      exclude: /\.(sa|sc|c)ss$/
-    })
-  ].filter(Boolean))
+  wc.plugins = wc.plugins.concat(
+    [
+      new webpack.DefinePlugin(conf.env || {}),
+      conf.banner &&
+        new webpack.BannerPlugin({
+          banner: conf.banner,
+          raw: true,
+          entryOnly: true,
+          exclude: /\.(sa|sc|c)ss$/,
+        }),
+    ].filter(Boolean)
+  );
+  [].concat(conf.presets || []).map((p) => {
+    const props = {
+      config: wc,
+      watch,
+    };
 
-  ;[].concat(conf.presets || [])
-    .map(p => {
-      const props = {
-        config: wc,
-        watch
-      }
+    try {
+      typeof p === "function"
+        ? p(props)
+        : Array.isArray(p)
+        ? require(`../presets/${p[0]}.js`)(p[1])(props)
+        : require(`../presets/${p}.js`)()(props);
+    } catch (e) {}
+  });
 
-      try {
-        typeof p === 'function' ? p(props) : (
-          Array.isArray(p) ? (
-            require(`../presets/${p[0]}.js`)(p[1])(props)
-          ) : (
-            require(`../presets/${p}.js`)()(props)
-          )
-        )
-      } catch (e) {}
-    })
-
-  return wc
-}
+  return wc;
+};
